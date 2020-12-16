3a8e76b4b75b2ee59a08370116b716f48cab0a1f
Grondag <grondag@gmail.com>
2020-12-15 22:19:46 -0800

    update frex events

--- src/main/java/grondag/renderbender/FrexEventTest.java
+++ src/main/java/grondag/renderbender/FrexEventTest.java
@@ -5,70 +5,78 @@ import grondag.renderbender.init.BasicBlocks;
 
 public class FrexEventTest {
 	private static boolean active = true;
+	private static boolean firstBlockOutline = true;
 
 	static void init() {
 		RenderBender.LOG.info("Registering FREX event test callbacks.");
 		RenderBender.LOG.info("Should see the following messages 1X in this order when world is loaded.");
-		RenderBender.LOG.info("    WorldRenderStartCallback");
-		RenderBender.LOG.info("    WorldRenderPostSetupCallback");
-		RenderBender.LOG.info("    WorldRenderPreEntityCallback");
-		RenderBender.LOG.info("    WorldRenderPostEntityCallback");
-		RenderBender.LOG.info("    WorldRenderDebugRenderCallback");
-		RenderBender.LOG.info("    WorldRenderPostTranslucentCallback");
-		RenderBender.LOG.info("    WorldRenderLastCallback");
-		RenderBender.LOG.info("    WorldRenderEndCallback");
+		RenderBender.LOG.info("    WorldRenderStart");
+		RenderBender.LOG.info("    WorldRenderAfterSetup");
+		RenderBender.LOG.info("    WorldRenderBeforeEntities");
+		RenderBender.LOG.info("    WorldRenderAfterEntities");
+		RenderBender.LOG.info("    WorldRenderBeforeDebugRender");
+		RenderBender.LOG.info("    WorldRenderAfterTranslucent");
+		RenderBender.LOG.info("    WorldRenderLast");
+		RenderBender.LOG.info("    WorldRenderEnd");
 
 		WorldRenderEvents.START.register(c -> {
 			if (active) {
-				RenderBender.LOG.info("WorldRenderStartCallback");
+				RenderBender.LOG.info("WorldRenderStart");
 			}
 		});
 
 		WorldRenderEvents.AFTER_SETUP.register(c -> {
 			if (active) {
-				RenderBender.LOG.info("WorldRenderPostSetupCallback");
+				RenderBender.LOG.info("WorldRenderAfterSetup");
 			}
 		});
 
 		WorldRenderEvents.BEFORE_ENTITIES.register(c -> {
 			if (active) {
-				RenderBender.LOG.info("WorldRenderPreEntityCallback");
+				RenderBender.LOG.info("WorldRenderBeforeEntities");
 			}
 		});
 
 		WorldRenderEvents.AFTER_ENTITIES.register(c -> {
 			if (active) {
-				RenderBender.LOG.info("WorldRenderPostEntityCallback");
+				RenderBender.LOG.info("WorldRenderAfterEntities");
 			}
 		});
 
-		WorldRenderEvents.BLOCK_OUTLINE.register(c -> {
-			if (c.blockState().getBlock() == BasicBlocks.GLOW_BLOCK_DYNAMIC) {
-            	c.cancelDefaultBlockOutline();
+		WorldRenderEvents.BEFORE_BLOCK_OUTLINE.register((c, h) -> {
+			if (firstBlockOutline) {
+				RenderBender.LOG.info("WorldRenderBeforeBlockOutline");
+				firstBlockOutline = false;
 			}
+
+			return true;
+		});
+
+		WorldRenderEvents.BLOCK_OUTLINE.register((c, b) -> {
+			return b.blockState().getBlock() != BasicBlocks.GLOW_BLOCK_DYNAMIC;
 		});
 
 		WorldRenderEvents.BEFORE_DEBUG_RENDER.register(c -> {
 			if (active) {
-				RenderBender.LOG.info("WorldRenderDebugRenderCallback");
+				RenderBender.LOG.info("WorldRenderBeforeDebugRender");
 			}
 		});
 
 		WorldRenderEvents.AFTER_TRANSLUCENT.register(c -> {
 			if (active) {
-				RenderBender.LOG.info("WorldRenderPostTranslucentCallback");
+				RenderBender.LOG.info("WorldRenderAfterTranslucent");
 			}
 		});
 
 		WorldRenderEvents.LAST.register(c -> {
 			if (active) {
-				RenderBender.LOG.info("WorldRenderLastCallback");
+				RenderBender.LOG.info("WorldRenderLast");
 			}
 		});
 
 		WorldRenderEvents.END.register(c -> {
 			if (active) {
-				RenderBender.LOG.info("WorldRenderEndCallback");
+				RenderBender.LOG.info("WorldRenderEnd");
 				active = false;
 			}
 		});
